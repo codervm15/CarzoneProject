055c937e7e9f820877e94daf6ec8710a4ac68d67
Vikrant Mehla <vikrantmehla@192.168.1.4>
2021-06-27 18:01:51 +0530

    updating project code

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 def cars(request):
     cars=Car.objects.order_by("-created_date")
 
-    data={"cars":cars,}
+    #Adding paginator
+    paginator= Paginator(cars, 2)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
+    data={"cars": paged_cars,}
     return render(request,'cars/cars.html',data)
 
 
@@ -16,3 +22,20 @@ def car_detail(request, id):
 
     data={'single_car':single_car,}
     return render(request,'cars/car_detail.html',data)
+
+
+#to implement search icon functionality on every page
+def search(request):
+
+    cars = Car.obejcts.order_by("-created_date")
+
+    if 'keyword' in request.GET:
+        keyword = request.GET["keyword"]
+        if keyword:
+            cars = cars.filter(description_icontains=keyword)
+
+    data = {'cars':cars,}
+
+    return render(request,'cars/search.html',data)
+
+
